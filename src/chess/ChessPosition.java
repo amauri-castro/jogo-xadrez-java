9eb090561035abdd83a25c277eda75db95dd3c76
amauri-castro <amauri.p.castro@gmail.com>
2023-04-05 21:53:00 -0300

    chess expection and chessposition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,33 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+	
+	private char column;
+	private int row;
+	
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Erro ao instanciar ChessPosition. Valores válidos são de a1 ate h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+
+	public int getRow() {
+		return row;
+	}
+
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	
+
+}
